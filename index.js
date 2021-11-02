abde5a066666f499f64af048626bdf6bca2c9ce3
Mauricio Robinson <63019039+MauricioRobinson@users.noreply.github.com>
2021-11-02 11:58:30 -0400

    Recibiendo parametros con get

--- index.js
+++ index.js
@@ -7,9 +7,75 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+
   res.json({
-    name: 'Product 1',
-    price: 1000,
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+app.get('/categories', (req, res) => {
+  res.json([
+    {
+      category: 'category1',
+    },
+    {
+      category: 'category2',
+    },
+    {
+      category: 'category3',
+    },
+  ]);
+});
+
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.json([
+    {
+      name: 'User1',
+    },
+    {
+      name: 'User2',
+    },
+  ]);
+});
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
   });
 });
 
