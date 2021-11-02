e18f5f6a7f134b75e2f05ff5b44c81374ddc60d5
Mauricio Robinson <63019039+MauricioRobinson@users.noreply.github.com>
2021-11-02 12:23:16 -0400

    Utilizando parametros query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -7,16 +8,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 2000,
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      napriceme: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
 
 app.get('/products/:id', (req, res) => {
@@ -61,14 +64,16 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-  res.json([
-    {
-      name: 'User1',
-    },
-    {
-      name: 'User2',
-    },
-  ]);
+  const { limit, offset } = req.query;
+
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay query params');
+  }
 });
 
 app.get('/users/:id', (req, res) => {
@@ -80,5 +85,5 @@ app.get('/users/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server running on port: ' + port);
+  console.log('Server running on port: ' + port + ' 👍');
 });
