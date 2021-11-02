1f58b8b4feb48e6f6b0783835dad5f73eb7a715a
Mauricio Robinson <63019039+MauricioRobinson@users.noreply.github.com>
2021-11-02 13:14:37 -0400

    Creando con POST

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const routerApi = require('./routes');
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello world from Express');
 });
