4bc0280f428e7a9a074ebc6676cedadcb68049b9
Mauricio Robinson <63019039+MauricioRobinson@users.noreply.github.com>
2021-11-08 23:03:27 -0500

    Modificando una entidad

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,18 +3,18 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const password = Joi.string().min(8);
 const email = Joi.string().email();
-// const rol = Joi.string().min(5);
+const role = Joi.string().min(5);
 
 const createUserSchema = Joi.object({
   password: password.required(),
   email: email.required(),
-  // rol: rol.required(),
+  role: role.required(),
 });
 
 const updateUserSchema = Joi.object({
   password,
   email,
-  // rol,
+  role,
 });
 
 const getUserSchema = Joi.object({
