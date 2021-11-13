0d77f98368d79c59bea0b355f16dd8b16495b699
Mauricio Robinson <63019039+MauricioRobinson@users.noreply.github.com>
2021-11-13 15:59:57 -0500

    Filtrando precios

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -6,6 +6,8 @@ const price = Joi.number().integer().min(10);
 const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
 
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
@@ -33,6 +35,17 @@ const getProductSchema = Joi.object({
 const queryProductSchema = Joi.object({
   limit,
   offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required(),
+  }),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema };
+module.exports = {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+  queryProductSchema,
+};
